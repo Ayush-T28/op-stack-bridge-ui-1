1a1d1754dedeaaa482173adc7bd5c3da6c512a2f
Ayush Trivedi <ayush.trivedi@zeeve.io>
2024-10-24 23:52:00 +0530

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ type HeaderProps = {
 }
 
 export default function Header({loaded}: HeaderProps) {
-    const config = getConfig();
+    const config = getConfig;
     const { mode, setMode } = useColorScheme();
     const location = useLocation();
     
@@ -27,7 +27,7 @@ export default function Header({loaded}: HeaderProps) {
     return (
         <nav style={{backgroundColor: mode === 'light' ? "rgba(255,255,255,0.7)" : 'rgba(0,0,0,0.7)', backdropFilter: 'blur(10px)'}}>
         <Stack direction='row' py={2} px={4} height={75} alignItems='center'>
-            <img src={mode === 'light' ?  (config.brand.logo.light ?? "/logo.svg") : (config.brand.logo.dark ?? "/logo_dark.svg")} height='50px' alt="Zeeve Logo"></img>
+            <img src={mode === 'light' ?  (config.brand.logo.light ?? "/logo.svg") : (config.brand.logo.dark ?? "/logo_dark.svg")} height='50px' alt={`${config.brand.name} Logo`}></img>
             <Stack direction='row' marginLeft={6} gap={3}>
                 <Typography color={location.pathname === '/' ? 'secondary' : 'InactiveCaptionText'} textTransform='uppercase' variant='button' letterSpacing={1} sx={{cursor: 'pointer', "&:hover": {color: 'secondary'}}}><Link to="/" style={{color: 'inherit', textDecoration: 'inherit'}}>Bridge</Link></Typography>
                <Typography color={location.pathname === '/activity' ?  'secondary' : 'InactiveCaptionText'} textTransform='uppercase' variant='button' letterSpacing={1} sx={{cursor: 'pointer', "&:hover": {color: 'secondary'}}}><Link to="/activity" style={{color: 'inherit', textDecoration: 'inherit'}}>Activity</Link></Typography>
